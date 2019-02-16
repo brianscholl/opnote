a23d45674bbe2120778e280f737a68e55e617b82
brianscholl <35386355+brianscholl@users.noreply.github.com>
2019-02-15 20:05:48 -0600

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -1,203 +1,204 @@
-(function(window){
-  window.extractData = function() {
-    var ret = $.Deferred();
+(function (window) {
+    window.extractData = function () {
+        var ret = $.Deferred();
 
-    function onError() {
-      console.log('Loading error', arguments);
-      ret.reject();
-    }
+        function onError() {
+            console.log('Loading error', arguments);
+            ret.reject();
+        }
 
-    function onReady(smart)  {
-      if (smart.hasOwnProperty('patient')) {
-        var patient = smart.patient;
-        var pt = patient.read();
-        var obv = smart.patient.api.fetchAll({
+        function onReady(smart) {
+            if (smart.hasOwnProperty('patient')) {
+                var patient = smart.patient;
+                var pt = patient.read();
+                var obv = smart.patient.api.fetchAll({
                     type: 'Observation',
                     query: {
-                      code: {
-                        $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
-                              'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
-                      }
+                        code: {
+                            $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
+                                'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
+                                'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                        }
                     }
-                  });
+                });
 
-        $.when(pt, obv).fail(onError);
+                $.when(pt, obv).fail(onError);
 
-        $.when(pt, obv).done(function(patient, obv) {
-          var byCodes = smart.byCodes(obv, 'code');
-          var gender = patient.gender;
-          var dob = new Date(patient.birthDate);
-          var day = dob.getDate();
-          var monthIndex = dob.getMonth() + 1;
-          var year = dob.getFullYear();
-            
-          var dobStr = monthIndex + '/' + day + '/' + year;
-          var fname = '';
-          var lname = '';
-          var identifier = patient.identifier[0].value;
-          var phone = patient.phone;
-          
-          if (typeof patient.name[0] !== 'undefined') {
-            fname = patient.name[0].given.join(' ');
-            lname = patient.name[0].family.join(' ');
-          }
+                $.when(pt, obv).done(function (patient, obv) {
+                    var byCodes = smart.byCodes(obv, 'code');
+                    var gender = patient.gender;
+                    var dob = new Date(patient.birthDate);
+                    var day = dob.getDate();
+                    var monthIndex = dob.getMonth() + 1;
+                    var year = dob.getFullYear();
 
-          
-          console.log('tokenResponse');
-          console.log(smart.tokenResponse);
-          console.log('tokenResponse.username');
-          console.log(smart.tokenResponse.username); 
-          console.log("smart.tokenResponse['id_token']");
-          console.log(smart.tokenResponse['id_token']); 
-           console.log('tokenResponse.id_token');
-          console.log(smart.tokenResponse.id_token); 
-          
-          
-          /*Get Provider Name out of id-token */
-             let b64DecodeUnicode = str =>
-                decodeURIComponent(
-                    Array.prototype.map.call(atob(str), c =>
-                        '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-                    ).join(''))
+                    var dobStr = monthIndex + '/' + day + '/' + year;
+                    var fname = '';
+                    var lname = '';
+                    var identifier = patient.identifier[0].value;
+                    var phone = patient.phone;
 
-            let parseJwt = token =>
-                JSON.parse(
-                    b64DecodeUnicode(
-                        token.split('.')[1].replace('-', '+').replace('_', '/')
-                    )
-                )
+                    if (typeof patient.name[0] !== 'undefined') {
+                        fname = patient.name[0].given.join(' ');
+                        lname = patient.name[0].family.join(' ');
+                    }
 
-            var dataProviderName = JSON.stringify(
-                parseJwt(smart.tokenResponse.id_token));
-            var dataProviderName1 = JSON.parse(dataProviderName);
-            var surgeon= dataProviderName1.name;   
-            $('#surgeon').text(surgeon);
-          
-          var height = byCodes('8302-2');
-          var systolicbp = getBloodPressureValue(byCodes('55284-4'),'8480-6');
-          var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
-          var hdl = byCodes('2085-9');
-          var ldl = byCodes('2089-1');
 
-          var p = defaultPatient();
-          p.birthdate = dobStr;
-          p.gender = gender;
-          p.fname = fname;
-          p.lname = lname;
-          p.age = parseInt(calculateAge(dob));
-          p.height = getQuantityValueAndUnit(height[0]);
-          p.identifier = identifier;
+                    console.log('tokenResponse');
+                    console.log(smart.tokenResponse);
+                    console.log('tokenResponse.username');
+                    console.log(smart.tokenResponse.username);
+                    console.log("smart.tokenResponse['id_token']");
+                    console.log(smart.tokenResponse['id_token']);
+                    console.log('tokenResponse.id_token');
+                    console.log(smart.tokenResponse.id_token);
 
-          if (typeof systolicbp != 'undefined')  {
-            p.systolicbp = systolicbp;
-          }
 
-          if (typeof diastolicbp != 'undefined') {
-            p.diastolicbp = diastolicbp;
-          }
+                    /*Get Provider Name out of id-token */
+                    let b64DecodeUnicode = str =>
+                        decodeURIComponent(
+                            Array.prototype.map.call(atob(str), c =>
+                                '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+                            ).join(''))
 
-          p.hdl = getQuantityValueAndUnit(hdl[0]);
-          p.ldl = getQuantityValueAndUnit(ldl[0]);
+                    let parseJwt = token =>
+                        JSON.parse(
+                            b64DecodeUnicode(
+                                token.split('.')[1].replace('-', '+').replace('_', '/')
+                            )
+                        )
 
-          ret.resolve(p);
-        });
-      } else {
-        onError();
-      }
-    }
+                    var dataProviderName = JSON.stringify(
+                        parseJwt(smart.tokenResponse.id_token));
+                    var dataProviderName1 = JSON.parse(dataProviderName);
+                    var surgeon = dataProviderName1.name;
+                    $('#token').html(p.dataProviderName);
+                    $('#surgeon').text(surgeon);
 
-    FHIR.oauth2.ready(onReady, onError);
-    return ret.promise();
+                    var height = byCodes('8302-2');
+                    var systolicbp = getBloodPressureValue(byCodes('55284-4'), '8480-6');
+                    var diastolicbp = getBloodPressureValue(byCodes('55284-4'), '8462-4');
+                    var hdl = byCodes('2085-9');
+                    var ldl = byCodes('2089-1');
 
-  };
+                    var p = defaultPatient();
+                    p.birthdate = dobStr;
+                    p.gender = gender;
+                    p.fname = fname;
+                    p.lname = lname;
+                    p.age = parseInt(calculateAge(dob));
+                    p.height = getQuantityValueAndUnit(height[0]);
+                    p.identifier = identifier;
 
-  function defaultPatient(){
-    return {
-      identifier: {value: ''},
-      fname: {value: ''},
-      lname: {value: ''},
-      gender: {value: ''},
-      birthdate: {value: ''},
-      age: {value: ''},
-      height: {value: ''},
-      systolicbp: {value: ''},
-      diastolicbp: {value: ''},
-      ldl: {value: ''},
-      hdl: {value: ''},
-      phone: {value: ''},
+                    if (typeof systolicbp != 'undefined') {
+                        p.systolicbp = systolicbp;
+                    }
+
+                    if (typeof diastolicbp != 'undefined') {
+                        p.diastolicbp = diastolicbp;
+                    }
+
+                    p.hdl = getQuantityValueAndUnit(hdl[0]);
+                    p.ldl = getQuantityValueAndUnit(ldl[0]);
+
+                    ret.resolve(p);
+                });
+            } else {
+                onError();
+            }
+        }
+
+        FHIR.oauth2.ready(onReady, onError);
+        return ret.promise();
+
     };
-  }
 
-  function getBloodPressureValue(BPObservations, typeOfPressure) {
-    var formattedBPObservations = [];
-    BPObservations.forEach(function(observation){
-      var BP = observation.component.find(function(component){
-        return component.code.coding.find(function(coding) {
-          return coding.code == typeOfPressure;
+    function defaultPatient() {
+        return {
+            identifier: { value: '' },
+            fname: { value: '' },
+            lname: { value: '' },
+            gender: { value: '' },
+            birthdate: { value: '' },
+            age: { value: '' },
+            height: { value: '' },
+            systolicbp: { value: '' },
+            diastolicbp: { value: '' },
+            ldl: { value: '' },
+            hdl: { value: '' },
+            phone: { value: '' },
+        };
+    }
+
+    function getBloodPressureValue(BPObservations, typeOfPressure) {
+        var formattedBPObservations = [];
+        BPObservations.forEach(function (observation) {
+            var BP = observation.component.find(function (component) {
+                return component.code.coding.find(function (coding) {
+                    return coding.code == typeOfPressure;
+                });
+            });
+            if (BP) {
+                observation.valueQuantity = BP.valueQuantity;
+                formattedBPObservations.push(observation);
+            }
         });
-      });
-      if (BP) {
-        observation.valueQuantity = BP.valueQuantity;
-        formattedBPObservations.push(observation);
-      }
-    });
 
-    return getQuantityValueAndUnit(formattedBPObservations[0]);
-  }
-
-  function isLeapYear(year) {
-    return new Date(year, 1, 29).getMonth() === 1;
-  }
-
-  function calculateAge(date) {
-    if (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())) {
-      var d = new Date(date), now = new Date();
-      var years = now.getFullYear() - d.getFullYear();
-      d.setFullYear(d.getFullYear() + years);
-      if (d > now) {
-        years--;
-        d.setFullYear(d.getFullYear() - 1);
-      }
-      var days = (now.getTime() - d.getTime()) / (3600 * 24 * 1000);
-      return years + days / (isLeapYear(now.getFullYear()) ? 366 : 365);
+        return getQuantityValueAndUnit(formattedBPObservations[0]);
     }
-    else {
-      return undefined;
-    }
-  }
 
-  function getQuantityValueAndUnit(ob) {
-    if (typeof ob != 'undefined' &&
-        typeof ob.valueQuantity != 'undefined' &&
-        typeof ob.valueQuantity.value != 'undefined' &&
-        typeof ob.valueQuantity.unit != 'undefined') {
-          return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
-    } else {
-      return undefined;
+    function isLeapYear(year) {
+        return new Date(year, 1, 29).getMonth() === 1;
     }
-  }
 
-  window.drawVisualization = function(p) {
-    $('#holder').show();
-    $('#loading').hide();
-    $('#MRN').html(p.identifier);
-    /*$('#phone').html(p.phone);*/
-    $('#fname').html(p.fname);
-    $('#lname').html(p.lname);
-    $('#gender').html(p.gender);
-    $('#birthdate').html(p.birthdate);
-    $('#age').html(p.age);
-    /*$('#height').html(p.height);
-    $('#systolicbp').html(p.systolicbp);
-    $('#diastolicbp').html(p.diastolicbp);
-    $('#ldl').html(p.ldl);
-    $('#hdl').html(p.hdl);*/
-    
-  };
-  
-  
-                 
-           
+    function calculateAge(date) {
+        if (Object.prototype.toString.call(date) === '[object Date]' && !isNaN(date.getTime())) {
+            var d = new Date(date), now = new Date();
+            var years = now.getFullYear() - d.getFullYear();
+            d.setFullYear(d.getFullYear() + years);
+            if (d > now) {
+                years--;
+                d.setFullYear(d.getFullYear() - 1);
+            }
+            var days = (now.getTime() - d.getTime()) / (3600 * 24 * 1000);
+            return years + days / (isLeapYear(now.getFullYear()) ? 366 : 365);
+        }
+        else {
+            return undefined;
+        }
+    }
+
+    function getQuantityValueAndUnit(ob) {
+        if (typeof ob != 'undefined' &&
+            typeof ob.valueQuantity != 'undefined' &&
+            typeof ob.valueQuantity.value != 'undefined' &&
+            typeof ob.valueQuantity.unit != 'undefined') {
+            return ob.valueQuantity.value + ' ' + ob.valueQuantity.unit;
+        } else {
+            return undefined;
+        }
+    }
+
+    window.drawVisualization = function (p) {
+        $('#holder').show();
+        $('#loading').hide();
+        $('#MRN').html(p.identifier);
+        *$('#phone').html(p.phone);
+        $('#fname').html(p.fname);
+        $('#lname').html(p.lname);
+        $('#gender').html(p.gender);
+        $('#birthdate').html(p.birthdate);
+        $('#age').html(p.age);
+        /*$('#height').html(p.height);
+        $('#systolicbp').html(p.systolicbp);
+        $('#diastolicbp').html(p.diastolicbp);
+        $('#ldl').html(p.ldl);
+        $('#hdl').html(p.hdl);*/
+
+    };
+
+
+
+
 
 })(window);
